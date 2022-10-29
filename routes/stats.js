5fecd74d5fe7518c571ec0201a4f9ca38378d5d5
MattThomas95 <matt.thomas95@gmail.com>
2022-10-29 00:06:49 -0400

    speed it up

--- routes/stats.js
+++ routes/stats.js
@@ -6,44 +6,50 @@ const getStats = async (req, res, next) => {
   let savedRespone;
   let newResponse;
   let oldRespone;
+  let intervalTime;
+  const res2 = res
   // const options = { method: "GET", headers: { accept: "application/json" } };
-  const interval = setInterval(() => {
-    const https = require("https");
-    const url =
-      "https://streamlabs.com/api/v1.0/donations?access_token=3p9XMiyCYlkZQT28dUGjE1EjfpogTKediFp2xPxc";
-    https
-      .get(url, (res) => {
-        let data = "";
-        res.on("data", (chunk) => {
-          data += chunk;
-        });
-        res.on("end", () => {
-          data = JSON.parse(data);
-          savedRespone = data["data"][0];
-          // console.log(savedRespone["data"][0]);
-        });
-      })
-      .on("error", (err) => {
-        console.log(err.message);
+  const https = require("https");
+  const url =
+    "https://streamlabs.com/api/v1.0/donations?access_token=3p9XMiyCYlkZQT28dUGjE1EjfpogTKediFp2xPxc";
+  https
+    .get(url, (res) => {
+      let data = "";
+      res.on("data", (chunk) => {
+        data += chunk;
       });
-    if (savedRespone) {
-      newResponse = savedRespone.donation_id;
-      console.log("newResponse", newResponse);
-      if (newResponse != oldRespone) {
-        try {
-          if (savedRespone.amount >= 20) {
-            const stats = savedRespone;
-            console.log("savedRespone", savedRespone);
-            res.json(stats);
-          }
-        } catch (e) {
-          next(e);
-        }
-      }
-      oldRespone = savedRespone.donation_id;
-    }
-  }, 10000);
-  return () => clearInterval(interval);
+      res.on("end", () => {
+        data = JSON.parse(data);
+        savedRespone = data["data"][0];
+        console.log(savedRespone);
+        res2.json(savedRespone);
+      });
+    })
+    .on("error", (err) => {
+      console.log(err.message);
+    });
+  // intervalTime = 10000;
+  // setInterval(function hello() {
+  // console.log(intervalTime);
+  // if (savedRespone) {
+  //   newResponse = savedRespone.donation_id;
+  //   console.log("newResponse", newResponse);
+  //   if (newResponse != oldRespone) {
+  //     try {
+  //       if (savedRespone.amount >= 20) {
+  //         const stats = savedRespone;
+  //         console.log("savedRespone", savedRespone);
+  //         res.json(stats);
+  //       }
+  //     } catch (e) {
+  //       next(e);
+  //     }
+  //   }
+  //   oldRespone = savedRespone.donation_id;
+  // }
+  // return hello;
+  // }(), intervalTime);
+  // return () => clearInterval(interval);
 };
 router.route("/api/v1/stats/").get(getStats);
 module.exports = router;
